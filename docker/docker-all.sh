7918321752500d7b5c5e25bf4bcb197430453a98
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-06-21 10:48:13 +0800

    fix docker-all

--- docker/docker-all.sh
+++ docker/docker-all.sh
@@ -468,3 +468,6 @@ do_install() {
 # wrapped up in a function so that we have some protection against only getting
 # half the file during "curl | sh"
 do_install
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
