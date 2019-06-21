6e362bf5daab98dddc802438babe9d15ada92743
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-06-21 11:29:29 +0800

    fix docker-all

--- docker/docker-all.sh
+++ docker/docker-all.sh
@@ -470,9 +470,9 @@ do_install() {
 do_install
 
 ###安装docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+chmod +x ~/docker-compose
+sudo mv ~/docker-compose /usr/local/bin/docker-compose
 ###给当前用户授权
 sudo groupadd docker
 sudo gpasswd -a ${USER} docker
