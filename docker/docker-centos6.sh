f94f2df5d411ce53ae9f2ec2dad6766baa47a343
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-06-24 10:15:30 +0800

    add docker-centos6

--- docker/docker-centos6.sh
+++ docker/docker-centos6.sh
@@ -1,6 +1,9 @@
-curl -sSL https://get.docker.com/ | sh
-systemctl start docker
-systemctl enable docker
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum update -y
+yum -y install docker-io
+service docker start
+chkconfig docker on
+
 
 ###安装docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
